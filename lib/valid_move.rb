cce49d164dd1fbcf10232e688787a7f3e4040da7
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-01 23:36:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index.between?(0,8)
+    then if position_taken? == false
     then true
+    else false
+    end
   else
     false
   end
